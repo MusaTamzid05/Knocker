b2ffa2d993ba8feac0c95b7fb1b34f2ba974e8ea
MusaTamzid05 <tamzid101@gmail.com>
2021-05-20 05:10:43 +0600

    image server added.

---
+++ image/server.go
@@ -0,0 +1,69 @@
+package image
+
+import (
+	"context"
+	"fmt"
+	"io/ioutil"
+	"knocker/docker_downloadpb"
+	"log"
+	"net"
+
+	"google.golang.org/grpc"
+)
+
+type TCPServer struct {
+	Address string
+	Port    int
+}
+
+type helperServer struct {
+	docker_downloadpb.UnimplementedDockerDownloadServiceServer
+}
+
+func (e *helperServer) Download(ctx context.Context, req *docker_downloadpb.DockerDownloadRequest) (*docker_downloadpb.DockerDownloadResponse, error) {
+	imageName := req.GetDockerDownload().GetImageName()
+
+	imageFound := false
+	buffer := []byte{}
+
+	buffer, err := e.readFile(imageName)
+
+	if err == nil {
+		imageFound = true
+	}
+
+	res := &docker_downloadpb.DockerDownloadResponse{
+		File:       buffer,
+		ImageFound: imageFound,
+		ImageName:  imageName,
+	}
+
+	return res, err
+
+}
+
+func (e *helperServer) LoadImage(imageName string) {
+	e.readFile(imageName)
+}
+
+func (e *helperServer) readFile(filepath string) ([]byte, error) {
+	return ioutil.ReadFile(filepath)
+}
+
+func (s *TCPServer) Start() {
+
+	serverURL := fmt.Sprintf("%s:%d", s.Address, s.Port)
+	listen, err := net.Listen("tcp", serverURL)
+
+	if err != nil {
+		log.Fatalf("Failed to listen: %v", err)
+	}
+
+	grpcServer := grpc.NewServer()
+	docker_downloadpb.RegisterDockerDownloadServiceServer(grpcServer, &helperServer{})
+
+	if err := grpcServer.Serve(listen); err != nil {
+		log.Fatalf("failed to server : %v", err)
+	}
+
+}
